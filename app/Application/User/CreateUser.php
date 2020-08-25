2802eebd4e9c30d685a3cc64e893ce494d58fb07
Miquel Rosselló Melis <rossellomelismiquel@gmail.com>
2020-08-25 20:49:39 +0200

    Implemented functionality to CreateUser use-case

--- app/Application/User/CreateUser.php
+++ app/Application/User/CreateUser.php
@@ -5,11 +5,30 @@ declare(strict_types=1);
 namespace App\Application\User;
 
 
+use App\Domain\User\User;
+use App\Domain\User\UserAlreadyExists;
+use App\Domain\User\UserRepository;
+use Carbon\Carbon;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+
 class CreateUser
 {
 
-    public function __construct(array $userData)
-    {
+    private UserRepository $userRepository;
+    private User $user;
 
+    public function __construct(UserRepository $userRepository, User $user)
+    {
+        $this->userRepository = $userRepository;
+        $this->user = $user;
+    }
+
+    public function execute(): void
+    {
+        try {
+            $this->userRepository->create($this->user);
+        } catch (UniqueConstraintViolationException $e) {
+            throw new UserAlreadyExists();
+        }
     }
 }
