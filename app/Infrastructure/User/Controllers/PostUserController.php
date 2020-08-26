598b069972ad21d5383528d40980473a9e9f24ef
Miquel Rosselló Melis <rossellomelismiquel@gmail.com>
2020-08-26 19:15:01 +0200

    Removed unused class

--- app/Infrastructure/User/Controllers/PostUserController.php
+++ app/Infrastructure/User/Controllers/PostUserController.php
@@ -6,15 +6,12 @@ namespace App\Infrastructure\User\Controllers;
 
 
 use App\Application\User\CreateUser;
-use App\Application\User\CreateUserRequestValidation;
 use App\Application\User\ParseUserFromJsonRequest;
 use App\Application\User\UserDataValidationException;
-use App\Domain\User\User;
 use App\Domain\User\UserAlreadyExists;
 use App\Infrastructure\User\Persistence\Doctrine\DoctrineUserRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Ramsey\Uuid\Uuid;
 
 class PostUserController
 {
