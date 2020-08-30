066a92883604e427ce5f030eadec43307acbe883
Miquel Rosselló Melis <rossellomelismiquel@gmail.com>
2020-08-30 18:35:09 +0200

    Created in-memory repository for tests

---
+++ tests/Infrastructure/User/Persistence/InMemoryUserRepository.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Tests\Infrastructure\User\Persistence;
+
+
+use App\Domain\User\User;
+use App\Domain\User\UserRepository;
+
+class InMemoryUserRepository implements UserRepository
+{
+
+    public array $users = array();
+
+    public function create(User $user): void
+    {
+        array_push($this->users, $user);
+    }
+
+    public function find(string $uuid): ?User
+    {
+        foreach ($this->users as $user) {
+            if ($user->getId() === $uuid)
+                return $user;
+        }
+
+        return null;
+    }
+}
